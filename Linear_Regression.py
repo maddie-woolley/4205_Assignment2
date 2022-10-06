21f5b0c2a6c396714851b7dd9909c663016c88d3
Maddie Woolley <95057458+maddie-woolley@users.noreply.github.com>
2022-10-06 17:38:33 -0500

    Update Linear_Regression.py
    Through Question 4 
    Still Missing 3C and 3E

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -54,8 +54,19 @@ model.fit(x_train, y_train)
 print(model.score(x_train, y_train))
 confMatrix = confusion_matrix(y_train, model.predict(x_train))
 print(confMatrix)
-sb.heatmap(confMatrix / np.sum(confMatrix), annot=True,
-           fmt='.2%', )
-plt.show()
+# sb.heatmap(confMatrix / np.sum(confMatrix), annot=True, fmt='.2%', )
+# plt.show()
 
 # Part 4 AUC /ROC Curve
+
+# define metrics
+y_pred_proba = model.predict_proba(x_test)[:,1]
+fpr, tpr, _ = metrics.roc_curve(y_test,  y_pred_proba)
+auc = metrics.roc_auc_score(y_test, y_pred_proba)
+
+# create ROC curve
+plt.plot(fpr,tpr,label="AUC="+str(auc))
+plt.ylabel('True Positive Rate')
+plt.xlabel('False Positive Rate')
+plt.legend(loc=4)
+plt.show()
