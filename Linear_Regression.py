abf2adf2dcd386fea449c57e8eadf0cb39491c22
Maddie Woolley <95057458+maddie-woolley@users.noreply.github.com>
2022-10-06 17:19:47 -0500

    Update Linear_Regression.py
    Through Part 4. Missing 3C and 3E

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -1,6 +1,8 @@
 # part 1-This is done
-
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
 
 diabetes = pd.read_csv('diabetes.csv')
 # imports data set and prints the main data, as well as the description of dataset
@@ -10,5 +12,50 @@ diabetes = pd.read_csv('diabetes.csv')
 # print(diabetes.describe())
 
 # part 2
-corrMatrix=diabetes.corr()
-print(corrMatrix)
+corrMatrix = diabetes.corr()
+# print(corrMatrix)
+
+# sb.heatmap(corrMatrix, annot=True)
+# plt.show()
+
+# part 3A - countplot
+# sb.countplot(x='Outcome', hue='Outcome', data=diabetes)
+# plt.legend(labels = ['Negative', 'Positive'])
+# plt.tight_layout()
+# plt.show()
+
+# part 3B - distribution plot
+# sb.displot(diabetes, kind="kde")
+# plt.show()
+
+# part 3C - box plot
+# sb.boxplot(diabetes)
+# plt.show()
+
+# part 3D - Pairplot
+# sb.pairplot(diabetes)
+# plt.show()
+
+# part 3E - Violin Plot
+# sb.violinplot(diabetes)
+# plt.show()
+
+# Part 4 -
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
+
+x = diabetes[['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness',
+              'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']]
+y = diabetes['Outcome']
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+model = LogisticRegression()
+model.fit(x_train, y_train)
+print(model.score(x_train, y_train))
+confMatrix = confusion_matrix(y_train, model.predict(x_train))
+print(confMatrix)
+sb.heatmap(confMatrix / np.sum(confMatrix), annot=True,
+           fmt='.2%', )
+plt.show()
+
+# Part 4 AUC /ROC Curve
